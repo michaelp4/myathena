c70d0250b2b8e5ece3f8a045a7f8ff2240c14b75
liorweitz <mm19924@gmail.com>
2020-09-07 23:54:51 +0300

    sdf

--- src/pgen/spin_cluster.cpp
+++ src/pgen/spin_cluster.cpp
@@ -59,12 +59,13 @@ void Cooling(AthenaArray<Real> &cons, const Real dt, Real k,Real j,Real i,
     return;
   }
   Real temperature = 72.8 * pressure / den;
-  if(Globals::E_floor > cons(IEN, k, j, i) - cooled_energy && rad < Globals::log_up_to_redius){
+  if(Globals::log_on > 0.0 && Globals::E_floor > cons(IEN, k, j, i) - cooled_energy){
      std::cout<< "*** Energy:" << cons(IEN, k, j, i)<< std::endl 
               << " Cooled energy:" << cooled_energy << std::endl
               << " Radoius: " << rad << " kpc" <<std::endl
               << " Time: " << time <<  std::endl
               << " Temperature: " << temperature << std::endl
+              << " log_up_to_redius: " << Globals::log_up_to_redius << std::endl
               << " dencity: " << den << " ***" << std::endl;
   }
   cons(IEN, k, j, i) = std::fmax(Globals::E_floor, cons(IEN, k, j, i) - cooled_energy);
