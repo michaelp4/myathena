018ab6aaf2b01d19f2e627a3a9e4f33aed27d3f4
liorweitz <mm19924@gmail.com>
2020-09-09 15:37:22 +0300

    sdf

--- src/pgen/spin_cluster.cpp
+++ src/pgen/spin_cluster.cpp
@@ -68,7 +68,7 @@ void Cooling(AthenaArray<Real> &cons, const AthenaArray<Real> &prim, const Real 
     std::cout << "***my_ rank: "<< Globals::my_rank <<  std::endl;
     std::cout << "radius: "<< rad << "***" << std::endl;
   }
-  cons(IEN, k, j, i) = std::fmax(Globals::E_floor + primative_kinetic_energy, pressure/gm1 + primative_kinetic_energy - primitive_cooled_energy);
+  // cons(IEN, k, j, i) = std::fmax(Globals::E_floor + primative_kinetic_energy, pressure/gm1 + primative_kinetic_energy - primitive_cooled_energy);
 
   // Real cons_den = cons(IDN, k, j, i);
   // Real conservative_momnetum_squared = SQR(cons(IM1,k,j,i)) + SQR(cons(IM2,k,j,i)) + SQR(cons(IM3,k,j,i));
