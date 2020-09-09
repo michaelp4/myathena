a6761deb4d46a68973fce1ff900b9b9194278e54
liorweitz <mm19924@gmail.com>
2020-09-09 16:36:36 +0300

    sdf

--- src/pgen/spin_cluster.cpp
+++ src/pgen/spin_cluster.cpp
@@ -69,12 +69,12 @@ void Cooling(AthenaArray<Real> &cons, const AthenaArray<Real> &prim, const Real 
   Real conservative_momnetum_squared = SQR(cons(IM1,k,j,i)) + SQR(cons(IM2,k,j,i)) + SQR(cons(IM3,k,j,i));
   Real conservative_kinetic_energy = 0.5*conservative_momnetum_squared/cons_den; 
 
-  // if (rad < 20  && time > 0.14 && time < 0.1418) {
-  //   std::cout << "***pressure: "<< pressure <<  std::endl;
-  //   std::cout << "***radius: "<< rad <<  std::endl;
-  //   std::cout << "***primitive_cooled_energy: "<< primitive_cooled_energy <<  std::endl;
-  //   std::cout << "***cons_IEN: "<< cons(IEN, k, j, i) <<  std::endl;
-  // }
+  if (conservative_kinetic_energy, cons(IEN, k, j, i) - primitive_cooled_energy) {
+    std::cout << "***pressure: "<< pressure <<  std::endl;
+    std::cout << "***radius: "<< rad <<  std::endl;
+    std::cout << "***primitive_cooled_energy: "<< primitive_cooled_energy <<  std::endl;
+    std::cout << "***cons_IEN: "<< cons(IEN, k, j, i) <<  std::endl;
+  }
   // cons(IEN, k, j, i) = std::fmax(Globals::E_floor + primative_kinetic_energy, pressure/gm1 + primative_kinetic_energy - primitive_cooled_energy);
   cons(IEN, k, j, i) = std::fmax(conservative_kinetic_energy, cons(IEN, k, j, i) - primitive_cooled_energy);
   // cons(IEN, k, j, i) -= primitive_cooled_energy;
