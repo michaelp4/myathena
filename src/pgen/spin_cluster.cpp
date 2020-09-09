cec4229c683e4c056ce14a4b037ca8ae0e37766d
liorweitz <mm19924@gmail.com>
2020-09-09 18:40:41 +0300

    sdf

--- src/pgen/spin_cluster.cpp
+++ src/pgen/spin_cluster.cpp
@@ -78,8 +78,8 @@ void Cooling(AthenaArray<Real> &cons, const AthenaArray<Real> &prim, const Real 
   //   std::cout << "***conservative_kinetic_energy: "<< conservative_kinetic_energy <<  std::endl;
   //   std::cout << "***time: "<< time <<  std::endl;
   // }
-  // cons(IEN, k, j, i) = std::fmax(Globals::E_floor + conservative_kinetic_energy, cons(IEN, k, j, i) - primitive_cooled_energy);
-  cons(IEN, k, j, i) = std::fmax(Globals::E_floor + conservative_kinetic_energy, pressure/gm1 + primative_kinetic_energy - primitive_cooled_energy);
+  cons(IEN, k, j, i) = std::fmax(Globals::E_floor + conservative_kinetic_energy, cons(IEN, k, j, i) - primitive_cooled_energy);
+  // cons(IEN, k, j, i) = std::fmax(Globals::E_floor + conservative_kinetic_energy, pressure/gm1 + primative_kinetic_energy - primitive_cooled_energy);
   
   // cons(IEN, k, j, i) = std::fmax(Globals::E_floor + primative_kinetic_energy, pressure/gm1 + primative_kinetic_energy - primitive_cooled_energy);
   
